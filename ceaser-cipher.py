e09dc4fba2fd0dec57711509047c867883efaab1
Nabendu <nghosh500@gmail.com>
2026-02-21 14:41:42 +0530

    Bug fixes for edge cases

--- ceaser-cipher.py
+++ ceaser-cipher.py
@@ -1,36 +1,39 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-message = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+run_again = "YES"
 
 def encrypt(original_text, shift_amount):
     encrypted_message = ""
     for letter in original_text:
-        start_index = alphabet.index(letter)
-        new_index = start_index + shift_amount
-        if new_index < 26:
-            shifted_letter = alphabet[new_index]
+        if letter in alphabet:
+            new_index = (alphabet.index(letter) + shift_amount) % 26
+            encrypted_message += alphabet[new_index]
         else:
-            shifted_letter = alphabet[new_index - 26]
-        encrypted_message += shifted_letter
+            encrypted_message += letter
+
     print(f"Your encoded message:\t {encrypted_message}")
 
 def decrypt(encrypted_text, shift_amount):
     decrypted_message = ""
     for letter in encrypted_text:
-        start_index = alphabet.index(letter)
-        new_index = start_index - shift_amount
-        if new_index > 0:
-            shifted_letter = alphabet[new_index]
+        if letter in alphabet:
+            new_index = (alphabet.index(letter) - shift_amount) % 26
+            decrypted_message += alphabet[new_index]
         else:
-            shifted_letter = alphabet[new_index + 26]
-        decrypted_message += shifted_letter
+            decrypted_message += letter
     print(f"Your decoded message:\t {decrypted_message}")
+
+while run_again == "YES":
+
+    action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    message = str(input("Type your message:\n")).lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if action == "encode":
+        encrypt(message, shift)
+    elif action == "decode":
+        decrypt(message, shift)
+    else:
+        print("You have typed wrong input")
     
-if action == "encode":
-    encrypt(message, shift)
-elif action == "decode":
-    decrypt(message, shift)
-else:
-    print("You have typed wrong input")
+    run_again = input("Type \"Yes\" to go again or \"No\" to exit:\t").upper()
