874f3c11f6817461fcea4dc4134c932c1e792409
Nabendu <nghosh500@gmail.com>
2026-03-19 09:53:18 +0530

    added ball class

--- OOP/pong_game/main.py
+++ OOP/pong_game/main.py
@@ -1,17 +1,31 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
-
+#Initialize screen/game
 screen = Screen()
-paddle = Paddle()
+left_paddle = Paddle(-350)
+right_paddle = Paddle(350)
+ball = Ball()
+game_on = True
 
+#Screen setup
 screen.setup(width=800,height=600)
 screen.title("PONG")
 screen.bgcolor("black")
+screen.tracer(0)
 
-
+#Paddle control
 screen.listen()
-screen.onkey(paddle.up,"Up")
-screen.onkey(paddle.down,"Down")
+screen.onkey(left_paddle.up,"w")
+screen.onkey(left_paddle.down,"s")
+screen.onkey(right_paddle.up,"Up")
+screen.onkey(right_paddle.down,"Down")
+
+while game_on:
+    time.sleep(.2)
+    screen.update()
+    ball.move()
 
 screen.exitonclick()
--- OOP/pong_game/paddle.py
+++ OOP/pong_game/paddle.py
@@ -1,16 +1,19 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
+        self.position = position
         self.color("white")
         self.shape("square")
         self.setheading(90)
         self.shapesize(stretch_len=5)
         self.penup()
-        self.goto(350,0)
+        self.goto(self.position,0)
 
     def up(self):
-        self.forward(20)
+        if self.ycor() < 240:
+            self.forward(20)
     def down(self):
-        self.backward(20)
+        if self.ycor() > -240:
+            self.backward(20)
