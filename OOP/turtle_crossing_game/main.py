c58ed9290e33f35b344762052c1368a2988c8561
Nabendu <nghosh500@gmail.com>
2026-03-19 12:05:59 +0530

    added main.py

---
+++ OOP/turtle_crossing_game/main.py
@@ -0,0 +1,15 @@
+from turtle import Screen
+
+
+# Set up screen
+screen = Screen()
+screen.bgcolor("white")
+screen.tracer(0)
+screen.setup(width=600,height=600)
+screen.title("Turtle Crossing Road")
+
+# Initialize game
+
+
+
+screen.exitonclick()
