23f38343bddd0e92bf73186a060c63149efd4bfb
Nabendu <nghosh500@gmail.com>
2026-03-22 10:59:47 +0530

    enhanced cars class

--- OOP/turtle_crossing_game/cars.py
+++ OOP/turtle_crossing_game/cars.py
@@ -2,7 +2,9 @@ from turtle import Turtle
 import random
 
 CAR_COLOURS = ("red","blue","green","yellow","purple","orange","cyan")
-START_XCORD = 300
+CAR_LENGTHS = (1.5, 2.0, 2.5)
+CAR_LANES = range(-250,260,25)
+START_XCORDS = (300,310,320,330)
 MOVE = 10
 
 class Cars():
@@ -12,10 +14,10 @@ class Cars():
     
     def generate_car(self):
         new_car = Turtle("square")
-        new_car.shapesize(stretch_len=1.5,stretch_wid=1)
+        new_car.shapesize(stretch_len=random.choice(CAR_LENGTHS),stretch_wid=1)
         new_car.color(random.choice(CAR_COLOURS))
         new_car.penup()
-        new_car.goto(START_XCORD,random.randint(-250,260))
+        new_car.goto(random.choice(START_XCORDS),random.choice(CAR_LANES))
         new_car.seth(180)
         self.cars.append(new_car)
     
