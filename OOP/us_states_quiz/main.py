13638b6591e186610b68b6939135b1eb93fb5845
Nabendu <nghosh500@gmail.com>
2026-04-06 13:28:47 +0530

    game complete

--- OOP/us_states_quiz/main.py
+++ OOP/us_states_quiz/main.py
@@ -7,11 +7,33 @@ screen.title("US States Game")
 screen.addshape(image)
 turtle.shape(image)
 
-user_input = screen.textinput("Guess the state", "Enter state name")
 states_data = pandas.read_csv("OOP/us_states_quiz/50_states.csv")
+states = states_data.state.to_list()
+guessed_states = []
+missing_states = []
 
-while True:
-    print(states_data[states_data.state == user_input.title()])
-    user_input = screen.textinput("Guess the state", "Enter state name")
+while len(guessed_states) < 50:
+    
+    user_input = screen.textinput(title=f"{len(guessed_states)}/50 states correctly guessed", prompt="Enter state name").title()
+    
+    if user_input == "Exit":
+        break
 
-screen.exitonclick()
+    elif user_input in states and user_input not in guessed_states:
+        state_data = states_data[states_data.state == user_input]
+        kurmo = turtle.Turtle()
+        kurmo.ht()
+        kurmo.penup()
+        kurmo.goto(state_data.x.item(),state_data.y.item())
+        kurmo.write(user_input)
+        guessed_states.append(user_input)
+
+if len(guessed_states) < 50:
+    print("Generating csv file with the states to learn")
+
+    for state in states:
+        if state not in guessed_states:
+            missing_states.append(state)
+    
+    new_df = pandas.DataFrame(missing_states)
+    new_df.to_csv("OOP/us_states_quiz/states_to_learn.csv")
