3c638018994a54f4eb86062da7d823f471608212
Nabendu <nghosh500@gmail.com>
2026-04-09 11:49:12 +0530

    ui design for password manager

---
+++ intermediate/password_manager/main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+# window.minsize(height=400,width=400)
+window.config(padx=50, pady=50)
+
+logo = PhotoImage(file="intermediate\password_manager\logo.png")
+canvas = Canvas(width=200,height=200, highlightthickness=0)
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0,column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1,column=0)
+website_entry = Entry(width=35)
+website_entry.grid(row=1,column=1,columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2,column=0)
+email_entry = Entry(width=35)
+email_entry.grid(row=2,column=1,columnspan=2)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3,column=0)
+password_entry = Entry(width=17)
+password_entry.grid(row=3,column=1)
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(row=3,column=2)
+
+add_button = Button(text="Add", width=30)
+add_button.grid(row=4, column=1,columnspan=2)
+
+
+window.mainloop()
